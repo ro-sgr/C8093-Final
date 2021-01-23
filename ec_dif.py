c86f8f0cccc2712205d2f8a408610b8802994d41
Ro Segura <rodriseguramo@gmail.com>
2021-01-23 12:31:06 -0600

    Updated ec_dif

--- ec_dif.py
+++ ec_dif.py
@@ -1,7 +1,17 @@
 import numpy as np
 
+# Parámetros constantes:
+k = 1.
+A = 1.
+B = 1.
+R = 1.
+I = 1.
+
 def calcular_thetadpt(ang, vel):
-    return - k/I * ang - (A*B)**2 / R * (np.cos(ang)) ** 2 * vel
+    ec_theta = (- k * ang )/I - ((A*B)**2)* (np.cos(ang)) ** 2 * vel / R 
+    return ec_theta
+
+THETA = []
 
 def f(t):
     # Valores iniciales
@@ -10,21 +20,12 @@ def f(t):
     dt = 0.01
     iteraciones = 0
     for tiempo in np.arange(0,t,dt):
-        iteraciones = iteraciones + 1
+        iteraciones += 1
         thetadpt = calcular_thetadpt(theta, thetapt)
         thetapt = thetapt + thetadpt * dt
         theta = theta + thetapt * dt
         if iteraciones % 10 == 0:
             THETA.append(theta)
-
-# Parámetros constantes:
-k = 1.
-A = 1.
-B = 1.
-R = 1.
-I = 1.
-
-THETA = []
 
 tiempo = int(input("Tiempo: "))
 f(tiempo)
