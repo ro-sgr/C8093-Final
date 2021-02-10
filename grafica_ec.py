e20dc560c1ae8e31a29a472708a6faec7d5b50e2
Ro Segura <rodriseguramo@gmail.com>
2021-02-10 17:25:48 -0600

    Gráfica

--- grafica_ec.py
+++ grafica_ec.py
@@ -54,26 +54,6 @@ while _run:
         TIEMPO = []
         THETA = []
 
-        def calcular_thetadpt(k, A, B, R, I, t, ang, vel):
-            ac_ang = (- k * ang )/I - (((A*B)**2)* ((np.cos(ang)) ** 2) * vel) / R # Cálculo de la aceleración angular según la ecuación diferencial
-            return ac_ang
-
-        def f(k, A, B, R, I, t):
-            # Valores iniciales
-            theta = 1.
-            thetapt = 0.
-            dt = 0.01 # Paso
-
-            for i in np.arange(0,t,dt):
-                TIEMPO.append(i) # Se adjunta el segundo a una lista
-
-                thetadpt = calcular_thetadpt(k, A, B, R, I, t, theta, thetapt) # Se calcula la aceleración angular según las variables
-                thetapt += thetadpt * dt # Como thetapt = d/dt (thetapt), theta_f = thetapt_i + thetadpt * dt, aprox
-                theta += thetapt * dt # Como thetapt = d/dt (theta), theta_f = thetapt_i + thetadpt * dt, aprox
-                THETA.append(theta)
-
-            return theta # Le agregué esto
-    
         print("----- GRAFICADORA -----\n")
 
         k = entrada_real(input("Constante del resorte de torsión: "))
@@ -83,14 +63,36 @@ while _run:
         I = entrada_real(input("Momento de incercia de la espira: "))
         t = entrada_real(input("Tiempo: "))
 
-        fig, ax = plt.subplots()
+        def calcular_thetadpt(ang, vel):
+            ac_ang = (- k * ang )/I - (((A*B)**2)* ((np.cos(ang)) ** 2) * vel) / R # Cálculo de la aceleración angular según la ecuación diferencial
+            return ac_ang
 
-        ax.grid(linestyle = "--")
+        def f(tiempo):
+            # Valores iniciales
+            theta = 1.
+            thetapt = 0.
+            dt = 0.01 # Paso
 
-        f(k, A, B, R, I, t)
+            for i in np.arange(0,tiempo,dt):
+                TIEMPO.append(i) # Se adjunta el segundo a una lista
+
+                thetadpt = calcular_thetadpt(theta, thetapt) # Se calcula la aceleración angular según las variables
+                thetapt += thetadpt * dt # Como thetapt = d/dt (thetapt), theta_f = thetapt_i + thetadpt * dt, aprox
+                theta += thetapt * dt # Como thetapt = d/dt (theta), theta_f = thetapt_i + thetadpt * dt, aprox
+                THETA.append(theta)
+
+            return theta # Le agregué esto
+
+        # FIGURA 1
+
+        fig1 = plt.figure()
+        ax1 = fig1.add_subplot(111)
+        ax1.grid(linestyle = "--")
+
+        f(t)
         cuadros = 1. / 20
 
-        def animacion(i):
+        def animacion1(i):
             j = int(i / (1./20))
             if j < 101:
                 t = TIEMPO[0:j]
@@ -102,12 +104,53 @@ while _run:
                 th = THETA[j-100:j]
                 a = TIEMPO[j-100]
                 b = TIEMPO[j+100]
-            ax.set_xlim(a,b) # Límites de la gráfica
-            ax.set_ylim(-5,5)
-            linea, = ax.plot(t, th, "r") 
+            ax1.set_xlim(a,b) # Límites de la gráfica
+            ax1.set_ylim(-5,5)
+            linea, = ax1.plot(t, th, "r") 
             return linea
 
-        animation = FuncAnimation(fig, func=animacion, frames=np.arange(0, 100, (1./20)),interval = 10) # Altérese el denominador del paso para cambiar los fps
+        animation1 = FuncAnimation(fig1, func=animacion1, frames=np.arange(0, 100, (1./20)),interval = 10) # Altérese el denominador del paso para cambiar los fps
+
+        # FIGURA 2
+
+        # Se definen dos listas que serán las entradas en X y en Y de los extremos del oscilador
+        x = [0,0]
+        y = [0,0]
+
+        fig2 = plt.figure()
+        ax2 = fig2.add_subplot(111)
+
+        # Límites de la gráfica
+        ax2.set_xlim(-2,2)
+        ax2.set_ylim(-2,2)
+
+        # Recta que hará de oscilador
+        linea, = ax2.plot(0, 0, "r")
+
+        # Recta horizontal
+        plt.axhline(0,-2,2,color="gray",linestyle="--")
+
+        def animacion2(i):
+            # Se definen las coordenadas de los extremos
+            x1 = np.cos(f(i))
+            y1 = np.sin(f(i))
+            x2 = -np.cos(f(i))
+            y2 = -np.sin(f(i))
+        
+            # Se alteran las entradas de las listas que definen los extremos de acuerdo a los valores recién calculados
+
+            x[0] = x1
+            y[0] = y1
+
+            x[1] = x2
+            y[1] = y2
+
+            linea.set_xdata(x) # Se dan los extremos en x del oscilador
+            linea.set_ydata(y) # Se dan los extremos en y del oscilador
+            
+            return linea
+
+        animation2 = FuncAnimation(fig2, func=animacion2, frames=np.arange(0, 100, (1./60)),interval = t)
 
         plt.show()
 
@@ -165,14 +208,6 @@ while _run:
         axk = plt.axes([0.25, .03, 0.50, 0.02])
         dk = Slider(axk, 'K', 0, 1, valinit=k_inicial)
 
-        # Deslizador A
-        axA = plt.axes([0.25, .001, 0.50, 0.02])
-        dA = Slider(axA, 'A', 0, 1, valinit=A_inicial)
-
-        # Deslizador B
-        axB = plt.axes([0.25, .001, 0.50, 0.02])
-        dB = Slider(axB, 'A', 0, 1, valinit=B_inicial)
-
         # Controles de animación
         is_manual = True
         intervalo = 100 # ms, tiempo entre cada cuadro de animación
@@ -197,32 +232,6 @@ while _run:
             is_manual=True
             update_k(val)
 
-        # A
-
-        def update_A(val):
-            # actualizar curva
-            l.set_ydata((- k_inicial * angulo )/I - (((val*B)**2)* ((np.cos(angulo)) ** 2) * vel_inicial) / R)
-            # redraw canvas while idle
-            fig.canvas.draw_idle()
-
-        def update_slider_A(val):
-            global is_manual
-            is_manual=True
-            update_A(val)
-
-        # A
-
-        def update_B(val):
-            # actualizar curva
-            l.set_ydata((- k_inicial * angulo )/I - (((A_inicial*val)**2)* ((np.cos(angulo)) ** 2) * vel_inicial) / R)
-            # redraw canvas while idle
-            fig.canvas.draw_idle()
-
-        def update_slider_B(val):
-            global is_manual
-            is_manual=True
-            update_B(val)
-
         # Global
 
         def update_plot(num):
@@ -233,25 +242,14 @@ while _run:
             val_k = (dk.val + scale) % dk.valmax
             dk.set_val(val_k)
 
-            val_A = (dA.val + scale) % dA.valmax
-            dA.set_val(val_A)
-
-            val_B = (dB.val + scale) % dB.valmax
-            dB.set_val(val_B)
-
             is_manual = False # the above line called update_slider, so we need to reset this
             return l,
 
         def on_click(event):
             (xm_k,ym_k),(xM_k,yM_k) = dk.label.clipbox.get_points()
-            (xm_A,ym_A),(xM_A,yM_A) = dA.label.clipbox.get_points()
+
             # k
             if xm_k < event.x < xM_k and ym_k < event.y < yM_k:
-                # Evento ocurre en el deslizador, ignorar ya que está siendo tratado por update_slider
-                return
-                
-            # A
-            elif xm_A < event.x < xM_A and ym_A < event.y < yM_A:
                 # Evento ocurre en el deslizador, ignorar ya que está siendo tratado por update_slider
                 return
 
@@ -261,8 +259,6 @@ while _run:
                 is_manual=False
 
         dk.on_changed(update_slider_k)
-        dA.on_changed(update_slider_A)
-        dB.on_changed(update_slider_B)
 
         fig.canvas.mpl_connect('button_press_event', on_click)
 
